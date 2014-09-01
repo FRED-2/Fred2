cd1f17249f0786ce3e1214b551733a85f366aaa6
b-schubert <schubert@informatik.uni-tuebingen.de>
2014-09-02 00:05:10 +0200

    Update __init__.py
    Added lost EpitopePredictionFactory

--- Prediction/__init__.py
+++ Prediction/__init__.py
@@ -45,3 +45,27 @@ class AEpitopePrediction(object):
         :return: Returns a Result object for the specified Peptides and Alleles
         """
         raise NotImplementedError
+
+        
+class EpitopePredictorFactory(object):
+    class __metaclass__(type):
+        def __init__(cls, name, bases, nmspc):
+            type.__init__(cls, name, bases, nmspc)
+
+        def __call__(self, _predictor, *args, **kwargs):
+            '''
+            just as I think it works.....
+
+            If a third person wants to write a new Epitope Predictior. He/She has to name the file fred_plugin and
+            inherit from AEpitopePrediction. That's it nothing more.
+            '''
+            try:
+                from fred_plugin import *
+            except ImportError:
+                pass
+
+            try:
+                return AEpitopePrediction.registry[_predictor](*args)
+            except KeyError:
+                raise ValueError("Predictor %s is not known. Please verify that such an Predictor is "%_predictor +
+                                 "supported by FRED2 and inherits AEpitopePredictor.")
