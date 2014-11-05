f1b42cf8a6aa7bea9ffcff3692d1d4a83c7f99df
b-schubert <schubert@informatik.uni-tuebingen.de>
2014-11-05 18:18:40 +0100

    changed prot_to_variation

--- Fred2/IO/MartsAdapter.py
+++ Fred2/IO/MartsAdapter.py
@@ -325,10 +325,10 @@ class MartsAdapter(ADBAdapter):
         rq_n = self.biomart_head%(_db, _dataset) \
                + self.biomart_filter%(filter, str(db_id)) \
                + self.biomart_filter%("germ_line_variation_source", "dbSNP") \
+               + self.biomart_attribute%("variation_name") \
                + self.biomart_attribute%("snp_chromosome_name") \
                + self.biomart_attribute%("chromosome_location") \
                + self.biomart_attribute%("allele") \
-               + self.biomart_attribute%("external_id") \
                + self.biomart_attribute%("snp_strand") \
                + self.biomart_attribute%("peptide_location") \
                + self.biomart_tail
