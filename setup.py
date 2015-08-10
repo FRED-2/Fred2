e69bcb3f666f9c537093fbb8271ffe68ffd879a1
mwalzer <walzer@informatik.uni-tuebingen.de>
2015-08-10 18:25:13 +0200

    fix issue #10 , removed empty Data/svms/MHC2Multi folder until we can push the models

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 from setuptools import setup, find_packages  # Always prefer setuptools over distutils
 from codecs import open  # To use a consistent encoding
 from os import path
+import glob
 
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     readme = f.read()
+
+data_files = list()
+directories = glob.glob('Fred2/Data/svms/*/')
+for directory in directories:
+    files = glob.glob(directory + '*')
+    data_files.append((directory, files))
+directories = glob.glob('Fred2/Data/examples/')
+for directory in directories:
+    files = glob.glob(directory + '*')
+    data_files.append((directory, files))
+
 
 setup(
     name='Fred2',
@@ -21,8 +33,8 @@ setup(
     url='https://github.com/Fred-2/Fred2',
 
     # Author details
-    author='Benjamin Schubert',
-    author_email='schubert@informatik.uni-tuebingen.de',
+    author='Benjamin Schubert, Mathias Walzer',
+    author_email='schubert@informatik.uni-tuebingen.de, walzer@informatik.uni-tuebingen.de',
 
     # Choose your license
     license='BSD',
@@ -33,7 +45,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Biologists, Pharmacologist, Developer',
@@ -55,20 +67,18 @@ setup(
     # documentation:
     packages=find_packages(exclude=['test', 'doc', 'tutorials']),
 
-    # Run-time dependencies. (will be installed by pip when FRED2 is installed)
-    install_requires=['pandas','pyomo>=4.0','biopython', 'svmlight', 'MySQL-python'],
-
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={
-        '': ['Data/*'],
-    },
+    #package_data={
+    #    '': ['Data/*'],
+    #},
+    #package_data is a lie: http://stackoverflow.com/questions/7522250/how-to-include-package-data-with-setuptools-distribute
 
     # 'package_data' is used to als install non package data files
     # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
     # example:
-    #data_files=[('exapl_target_dir', ['data/file1','data/file2'])],
+    data_files=data_files,
 
     # Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
@@ -79,4 +89,8 @@ setup(
             'epitopeprediction=Fred2.Apps.EpitopePrediction:main',
         ],
     },
+
+    # Run-time dependencies. (will be installed by pip when FRED2 is installed)
+    install_requires=['pandas', 'pyomo>=4.0', 'biopython', 'svmlight', 'MySQL-python >= 1.2.4'],
+
 )
