b0f16fe4476bb8748dbaddebfa18f51e93bfe563
b-schubert <benni.schubert@gmail.com>
2018-08-24 08:09:16 +0200

    Bump Version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     name='Fred2',
 
     # Version:
-    version='2.0.1',
+    version='2.0.3',
 
     description='A Framework for Epitope Detection and Vaccine Design',
     long_description=readme,
