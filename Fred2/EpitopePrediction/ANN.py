c8cfae867d93f2c1f46c2e1200b2c14c1dbd1146
Zethson <lukas.heumos@gmail.com>
2019-05-13 19:41:10 +0200

    [FIX] mhcflurry allele formatting

--- Fred2/EpitopePrediction/ANN.py
+++ Fred2/EpitopePrediction/ANN.py
@@ -365,23 +365,17 @@ class MHCFlurryPredictor(AANNEpitopePrediction):
     """
     __alleles = frozenset(
         ["HLA-A*01:01", "HLA-A*02:01", "HLA-A*02:02", "HLA-A*02:03", "HLA-A*02:05", "HLA-A*02:06", "HLA-A*02:07",
-         "HLA-A*02:11", "HLA-A*02:12",
-         "HLA-A*02:16", "HLA-A*02:17", "HLA-A*02:19", "HLA-A*02:50", "HLA-A*03:01", "HLA-A*11:01", "HLA-A*23:01",
-         "HLA-A*24:02", "HLA-A*24:03",
-         "HLA-A*25:01", "HLA-A*26:01", "HLA-A*26:02", "HLA-A*26:03", "HLA-A*29:02", "HLA-A*30:01", 'HLA-A*30:02',
-         "HLA-A*31:01", "HLA-A*32:01",
-         "HLA-A*33:01", "HLA-A*66:01", "HLA-A*68:01", "HLA-A*68:02", "HLA-A*68:23", "HLA-A*69:01", "HLA-A*80:01",
-         "HLA-B*07:01", "HLA-B*07:02",
-         "HLA-B*08:01", "HLA-B*08:02", "HLA-B*08:03", "HLA-B*14:02", "HLA-B*15:01", "HLA-B*15:02", "HLA-B*15:03",
-         "HLA-B*15:09", "HLA-B*15:17",
-         "HLA-B*18:01", "HLA-B*27:02", "HLA-B*27:03", "HLA-B*27:04", "HLA-B*27:05", "HLA-B*27:06", "HLA-B*35:01",
-         "HLA-B*35:03", "HLA-B*37:01",
-         "HLA-B*38:01", "HLA-B*39:01", "HLA-B*39:06", "HLA-B*40:01", "HLA-B*40:02", "HLA-B*42:01", "HLA-B*44:02",
-         "HLA-B*44:03", "HLA-B*45:01",
+         "HLA-A*02:11", "HLA-A*02:12", "HLA-A*02:16", "HLA-A*02:17", "HLA-A*02:19", "HLA-A*02:50", "HLA-A*03:01",
+         "HLA-A*11:01", "HLA-A*23:01", "HLA-A*24:02", "HLA-A*24:03", "HLA-A*25:01", "HLA-A*26:01", "HLA-A*26:02",
+         "HLA-A*26:03", "HLA-A*29:02", "HLA-A*30:01", 'HLA-A*30:02', "HLA-A*31:01", "HLA-A*32:01", "HLA-A*33:01",
+         "HLA-A*66:01", "HLA-A*68:01", "HLA-A*68:02", "HLA-A*68:23", "HLA-A*69:01", "HLA-A*80:01", "HLA-B*07:01",
+         "HLA-B*07:02", "HLA-B*08:01", "HLA-B*08:02", "HLA-B*08:03", "HLA-B*14:02", "HLA-B*15:01", "HLA-B*15:02",
+         "HLA-B*15:03", "HLA-B*15:09", "HLA-B*15:17", "HLA-B*18:01", "HLA-B*27:02", "HLA-B*27:03", "HLA-B*27:04",
+         "HLA-B*27:05", "HLA-B*27:06", "HLA-B*35:01", "HLA-B*35:03", "HLA-B*37:01", "HLA-B*38:01", "HLA-B*39:01",
+         "HLA-B*39:06", "HLA-B*40:01", "HLA-B*40:02", "HLA-B*42:01", "HLA-B*44:02", "HLA-B*44:03", "HLA-B*45:01",
          "HLA-B*46:01", "HLA-B*48:01", "HLA-B*51:01", "HLA-B*53:01", "HLA-B*54:01", "HLA-B*57:01", "HLA-B*58:01",
-         "HLA-B*83:01", "HLA-C*03:03",
-         "HLA-C*04:01", "HLA-C*05:01", "HLA-C*06:02", "HLA-C*07:02", "HLA-C*08:02", "HLA-C*12:03", "HLA-C*14:02",
-         "HLA-C*15:02"])
+         "HLA-B*83:01", "HLA-C*03:03", "HLA-C*04:01", "HLA-C*05:01", "HLA-C*06:02", "HLA-C*07:02", "HLA-C*08:02",
+         "HLA-C*12:03", "HLA-C*14:02", "HLA-C*15:02"])
     __supported_length = [8, 9, 10, 11, 12, 13, 14, 15]
     __name = "mhcflurry"
     __version = "1.2.2"
