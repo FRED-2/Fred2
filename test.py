49329cbf70b6d70bb8bbf4ebd6e84f39b6481a8f
Zethson <lukas.heumos@gmail.com>
2019-05-20 23:28:20 +0200

    [FIX] removed test script

--- test.py
+++
@@ -1,6 +0,0 @@
-from Fred2.EpitopePrediction import EpitopePredictorFactory, Peptide
-
-peptides = [Peptide("SYFPEITHI"), Peptide("FIASNGVKL"), Peptide("LLGATCMFV")]
-print EpitopePredictorFactory("mhcflurry").predict(peptides)
-print EpitopePredictorFactory("mhcnuggets-class-1").predict(peptides)
-print EpitopePredictorFactory("mhcnuggets-class-2").predict(peptides)
